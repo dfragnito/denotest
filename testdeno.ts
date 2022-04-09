30bca61f3eca23c9ee2de2d657bbb346ebd24d6c
dfragnito <dean@xornet.com>
2022-04-09 09:03:52 -0400

    Update testdeno.ts

--- testdeno.ts
+++ testdeno.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 async function handler(req: Request): Promise<Response> {
-  //const data = await Deno.readFile("hello.txt");
+  const data = await Deno.readFile("./appendpersons.json");
   const resp = await fetch("https://staging.demo.api.sfsql.io/w9p8eajl/api/v1/run", {
     // The init object here has an headers object containing a
     // header that indicates what type of response we accept.
@@ -12,7 +12,7 @@ async function handler(req: Request): Promise<Response> {
        "content-type": "application/json",
        "x-sfsql-apikey": Deno.env.get('api_key')
     },
-    body: JSON.stringify(await Deno.readFile("./appendpersons.json"))
+    body: JSON.stringify(data)
   });
   return new Response(resp.body, {
     status: resp.status,
