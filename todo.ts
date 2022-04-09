6373e15ba9e9afc22ab28fea27f8b4e30600a44e
dfragnito <dean@xornet.com>
2022-04-09 19:53:04 -0400

    Update todo.ts

--- todo.ts
+++ todo.ts
@@ -1,4 +1,4 @@
-import { listenAndServe } from "https://deno.land/std@0.114.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 import data from "./todo.json" assert { type: "json" };
 
