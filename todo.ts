b9b2b24248c334e2e3ae84fd0350519275a1b026
dfragnito <dean@xornet.com>
2022-04-09 19:16:02 -0400

    Create todo.ts

---
+++ todo.ts
@@ -0,0 +1,21 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import data from "./todo.json" assert { type: "json" };
+async function handler(req: Request): Promise<Response> {
+   
+  const resp = await fetch("https://staging.api.sfsql.io/ijhe3q1p/api/v1/run", {
+    method: "POST",
+    headers: {
+       "content-type": "application/json",
+       "x-sfsql-apikey": Deno.env.get('api_key')
+    },
+    body: JSON.stringify(data)
+  });
+  return new Response(resp.body, {
+    status: resp.status,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
+console.log("Listening on http://localhost:8000");
+serve(handler);
