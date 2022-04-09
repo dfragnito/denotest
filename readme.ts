013641babaf38eef0bb3a488affac9aaf5c173df
dfragnito <dean@xornet.com>
2022-04-09 14:05:43 -0400

    Update readme.ts

--- readme.ts
+++ readme.ts
@@ -1,9 +1,4 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-async function handler(_req) {
-  const readme = await Deno.readTextFile("./appendpersons.json");
-  event.respondWith(new Response(readme));
-}
-
-console.log("Listening on http://localhost:8000");
-serve(handler);
+import data from "./appendpersons.json" assert { type: "json" };
+console.log(data);
