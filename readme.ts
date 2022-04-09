b3344dda94bb26a244c3bae8c87b1f87c9a1b940
dfragnito <dean@xornet.com>
2022-04-09 13:55:23 -0400

    Update readme.ts

--- readme.ts
+++ readme.ts
@@ -1,16 +1,8 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 async function handler(_req) {
-  // Let's read the README.md file available at the root
-  // of the repository to explore the available methods.
-
-  // Relative paths are relative to the root of the repository
-  const readmeRelative = await Deno.readFile("./appendpersons.json");
-  );
-
-  // Decode the Uint8Array as string.
-  const readme = new TextDecoder().decode(readmeRelative);
-  return new Response(readme);
+  const readme = await Deno.readTextFile("./appendpersons.json");
+  event.respondWith(new Response(readme));
 }
 
 console.log("Listening on http://localhost:8000");
