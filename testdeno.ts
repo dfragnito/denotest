fca6b892afcd762363daa4cfec7002851075ac81
dfragnito <dean@xornet.com>
2022-04-09 15:09:00 -0400

    Update testdeno.ts

--- testdeno.ts
+++ testdeno.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 import data from "./todo.json" assert { type: "json" };
 
-async function handler(req: Request): Promise<Response> {
+async function handler(req: Request): Promise<Response> { 
    
   const resp = await fetch("https://staging.api.sfsql.io/ijhe3q1p/api/v1/run", {
     method: "POST",
