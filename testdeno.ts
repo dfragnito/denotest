c5c55685443d8f7909bac749b7e7be8bf136e40d
dfragnito <dean@xornet.com>
2022-04-09 13:47:42 -0400

    Update testdeno.ts

--- testdeno.ts
+++ testdeno.ts
@@ -4,11 +4,7 @@ async function handler(req: Request): Promise<Response> {
    const decoder = new TextDecoder('utf-8')
    const data = await Deno.readFile('./appendpersons.json')
   const resp = await fetch("https://staging.demo.api.sfsql.io/w9p8eajl/api/v1/run", {
-    // The init object here has an headers object containing a
-    // header that indicates what type of response we accept.
-    // We're not specifying the method field since by default
-    // fetch makes a GET request.
-     method: "POST",
+    method: "POST",
     headers: {
        "content-type": "application/json",
        "x-sfsql-apikey": Deno.env.get('api_key')
