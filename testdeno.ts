848381cfd22ae23922cee22152441250f885eac6
dfragnito <dean@xornet.com>
2022-04-09 09:26:08 -0400

    Update testdeno.ts

--- testdeno.ts
+++ testdeno.ts
@@ -1,17 +1,8 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-async function handler(_req) {
-   // Relative paths are relative to the root of the repository
-  const jsonRelative = await Deno.readFile("./appendpersons.json");
-  );
-
-  // Decode the Uint8Array as string.
-  const json = new TextDecoder().decode(jsonRelative);
-  return new Response(json);
-}
-
 async function handler(req: Request): Promise<Response> {
-  const data = await Deno.readFile("./appendpersons.json");
+   const decoder = new TextDecoder('utf-8')
+   const data = await Deno.readFile('./appendpersons.json')
   const resp = await fetch("https://staging.demo.api.sfsql.io/w9p8eajl/api/v1/run", {
     // The init object here has an headers object containing a
     // header that indicates what type of response we accept.
@@ -22,7 +13,7 @@ async function handler(req: Request): Promise<Response> {
        "content-type": "application/json",
        "x-sfsql-apikey": Deno.env.get('api_key')
     },
-    body: JSON.stringify(handler)
+    body: JSON.stringify(decoder.decode(data))
   });
   return new Response(resp.body, {
     status: resp.status,
