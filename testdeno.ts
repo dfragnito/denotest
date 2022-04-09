a40bdfb0a8f6b8a27c66edeff1d527bf12d63769
dfragnito <dean@xornet.com>
2022-04-09 08:09:18 -0400

    Create testdeno.ts

---
+++ testdeno.ts
@@ -0,0 +1,71 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+async function handler(req: Request): Promise<Response> {
+  const resp = await fetch("https://staging.demo.api.sfsql.io/w9p8eajl/api/v1/run", {
+    // The init object here has an headers object containing a
+    // header that indicates what type of response we accept.
+    // We're not specifying the method field since by default
+    // fetch makes a GET request.
+     method: "POST",
+    headers: {
+       "content-type": "application/json",
+       "x-sfsql-apikey":"YfvGrfalI2qmGA2xQPMr4Zl1SMX9UIfbcI6AmhPaXpzcwKW9QJ4c0ttRyGv8Ruvk"
+    },
+    body: JSON.stringify([
+	{
+		"*delete": {
+			"objfilter": "SELECT $o:Person.attrset('delete')",
+			"_comment": "prepare example by marking previous root Person objects and all their children as 'deleted'"
+		}
+	},
+	{
+		"*purge": {},
+		"_comment": "purge all objects marked for deletion."
+	},
+	{
+		"modify": {
+			"data": {
+				"Person": [
+					{
+						"#append": {}
+					},
+					{
+						"fname": "Sue",
+						"lname": "Smith",
+						"age": 30
+					},
+					{
+						"fname": "Tom",
+						"lname": "Jones",
+						"age": 55
+					}
+				]
+			},
+			"_comment": "Add 2 Person objects"
+		}
+	},
+	{
+		"query": {
+			"sfsql": "SELECT $o:Person.oid() as 'oid', $s:Person.fname as 'fname', $s:Person.lname as 'lname', $i:Person.age as 'age'",
+			"_comment": "Query the oid, fname, lname and age of the added Persons"
+		}
+	}
+]
+
+
+
+
+
+
+    )
+  });
+  return new Response(resp.body, {
+    status: resp.status,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
+
+console.log("Listening on http://localhost:8000");
+serve(handler);
